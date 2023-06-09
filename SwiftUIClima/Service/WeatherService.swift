c1ca2ce4be1750b401830d66a7d969ebdca0c483
antoniocasto <mail.antoniocasto@gmail.com>
2023-06-09 11:03:55 +0200

    Add Weather Service struct and .gitignore

---
+++ SwiftUIClima/Service/WeatherService.swift
@@ -0,0 +1,19 @@
+//
+//  WeatherService.swift
+//  SwiftUIClima
+//
+//  Created by Antonio Casto on 09/06/23.
+//
+
+import Foundation
+import CoreLocation
+
+struct WeatherService {
+        
+    static func fetchWeatherByCoordinate(_ coordinate: CLLocationCoordinate2D) async throws {
+        
+        
+        
+    }
+    
+}
