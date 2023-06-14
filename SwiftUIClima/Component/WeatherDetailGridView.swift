7f81c21535d4bea7fcab1d33cbd0c703c3c684c0
antoniocasto <mail.antoniocasto@gmail.com>
2023-06-14 12:25:42 +0200

    Add WeatherDetailGridView locales

--- SwiftUIClima/Component/WeatherDetailGridView.swift
+++ SwiftUIClima/Component/WeatherDetailGridView.swift
@@ -16,15 +16,28 @@ struct WeatherDetailGridView: View {
     var body: some View {
         
         LazyVGrid(columns: columns, spacing: 16) {
-            CardView(title: "Sunrise", systemIcon: "sunrise", value: "06:20")
-            CardView(title: "Sunset", systemIcon: "sunset", value: "20:30")
-            CardView(title: "Wind Speed", systemIcon: "wind", value: "7 km/h")
-            CardView(title: "Feels Like", systemIcon: "thermometer.sun", value: "27°")
-            CardView(title: "Humidity", systemIcon: "humidity", value: "60%")
-            CardView(title: "Visibility", systemIcon: "eye.fill", value: "26 km")
-            CardView(title: "Pressure", systemIcon: "barometer", value: "1.011 hPa")
-            CardView(title: "Sea Level", systemIcon: "water.waves", value: "140 m")
+            CardView(title: WeatherDetailGridView.sunrise, systemIcon: "sunrise", value: "06:20")
+            CardView(title: WeatherDetailGridView.sunset, systemIcon: "sunset", value: "20:30")
+            CardView(title: WeatherDetailGridView.wind, systemIcon: "wind", value: "7 km/h")
+            CardView(title: WeatherDetailGridView.feelsLike, systemIcon: "thermometer.sun", value: "27°")
+            CardView(title: WeatherDetailGridView.humidity, systemIcon: "humidity", value: "60%")
+            CardView(title: WeatherDetailGridView.visibility, systemIcon: "eye.fill", value: "26 km")
+            CardView(title: WeatherDetailGridView.pressure, systemIcon: "barometer", value: "1.011 hPa")
+            CardView(title: WeatherDetailGridView.seaLevel, systemIcon: "water.waves", value: "140 m")
         }
         
     }
+}
+
+extension WeatherDetailGridView {
+    static let sunrise = LocalizedStringKey("WeatherDetailGridView.sunrise")
+    static let sunset = LocalizedStringKey("WeatherDetailGridView.sunset")
+    static let wind = LocalizedStringKey("WeatherDetailGridView.wind")
+    static let feelsLike = LocalizedStringKey("WeatherDetailGridView.feelsLike")
+    static let humidity = LocalizedStringKey("WeatherDetailGridView.humidity")
+    static let visibility = LocalizedStringKey("WeatherDetailGridView.visibility")
+    static let pressure = LocalizedStringKey("WeatherDetailGridView.pressure")
+    static let seaLevel = LocalizedStringKey("WeatherDetailGridView.seaLevel")
+
+
 }
