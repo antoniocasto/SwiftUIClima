fd8f1e4981e68f8b03d6075edcd9cfa40abe255b
antoniocasto <mail.antoniocasto@gmail.com>
2023-07-03 11:09:56 +0200

    Add app speed model

---
+++ SwiftUIClima/Model/AppSpeed.swift
@@ -0,0 +1,34 @@
+//
+//  AppSpeed.swift
+//  SwiftUIClima
+//
+//  Created by Antonio Casto on 03/07/23.
+//
+
+import SwiftUI
+
+/// Enum used to select the app speed unit measure.
+enum AppSpeed: Int, CaseIterable, Hashable {
+    
+    case metreSec
+    case milesHour
+    
+    // Used for UserDefaults
+    static let preferenceKey = "AppSpeed"
+    
+    // Speed full name
+    var speedName: LocalizedStringKey {
+        switch self {
+        case .metreSec:
+            return Self.metreSecSpeed
+        case .milesHour:
+            return Self.milesHourSpeed
+        }
+    }
+    
+}
+
+extension AppSpeed {
+    static let metreSecSpeed = LocalizedStringKey("AppSpeed.metreSecSpeed")
+    static let milesHourSpeed = LocalizedStringKey("AppSpeed.milesHourSpeed")
+}
